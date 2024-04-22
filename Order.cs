cd633463784b3e9d2187ab13dffd98e41ab9ac7e
Manaila Robert <robert122003@gmail.com>
2024-04-22 20:31:18 +0300

    Finished

--- Order.cs
+++ Order.cs
@@ -13,17 +13,28 @@ namespace PizzeriaClasses
         public int AdressID { get; set; }
         public string PaymentMethod { get; set; }
 
+        public double OrderPrice {  get; set; }
+        public string OrderStatus { get; set; }
+        public DateTime OrderDate { get; set; }
         public Customers Customer { get; set; }
         public Adress Adress { get; set; }
         public  ICollection<OrderBeverage> OrderBeverages { get; set; }
         public ICollection<OrderPizza> OrderPizza { get; set;}
     
     
-        public Order(int customerID, int adressID,string paymentMethod)
+        public Order(int customerID, int adressID,string paymentMethod, double orderPrice,DateTime orderDate ,string orderStatus = "active")
         {
             CustomerID = customerID;
             AdressID = adressID;
             PaymentMethod = paymentMethod;
+            OrderStatus = orderStatus;
+            OrderPrice = orderPrice;
+            OrderDate = orderDate;
+        }
+
+        public override string ToString()
+        {
+            return $"#{OrderID}";
         }
     }
 }
